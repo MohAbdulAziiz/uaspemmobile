bbbe4a1225d6200801e53acc166545949d82fdd2
MOH. ABDUL AZIZ <148872208+MohAbdulAziiz@users.noreply.github.com>
2025-06-23 09:52:50 +0700

    Update pimpinanlayout.js

--- src/components/pimpinanlayout.js
+++ src/components/pimpinanlayout.js
@@ -9,9 +9,10 @@ import {
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 )
+
 
 export default function AdminLayout({ children }) {
   const [isCollapsed, setIsCollapsed] = useState(false)
