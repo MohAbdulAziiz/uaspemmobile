7b274e367454dd4b5d2d9c01d51e2af62065b421
MOH. ABDUL AZIZ <148872208+MohAbdulAziiz@users.noreply.github.com>
2025-06-23 10:05:55 +0700

    Update adminlayout.js

--- src/components/adminlayout.js
+++ src/components/adminlayout.js
@@ -10,9 +10,8 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_ANON_KEY,
 )
-
 
 export default function AdminLayout({ children }) {
   const [isCollapsed, setIsCollapsed] = useState(false)
