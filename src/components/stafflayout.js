36c97916f3f2f686b3e81ba5f3730f0f792e4c48
MOH. ABDUL AZIZ <148872208+MohAbdulAziiz@users.noreply.github.com>
2025-06-23 09:53:12 +0700

    Update stafflayout.js

--- src/components/stafflayout.js
+++ src/components/stafflayout.js
@@ -9,8 +9,8 @@ import {
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 )
 
 export default function AdminLayout({ children }) {
